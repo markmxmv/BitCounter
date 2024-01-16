8a8aff4c6aad30bfd4b58f2180499d0436d0b13f
Mark Maximov <markstopkidding@gmail.com>
2024-01-16 23:07:58 +0100

    Trying to fetch data from CMC and installing axios

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import css from 'rollup-plugin-import-css';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import rollupNodeResolve from 'rollup-plugin-node-resolve'
+import rollupJson from 'rollup-plugin-json'
 
 export default {
 	input: "src/app.js",
@@ -7,5 +9,6 @@ export default {
 		dir: "dist",
 		format: "esm"
 	},
-	plugins: [css({output: "bundle.css"}), nodeResolve()]
+	plugins: [css({output: "bundle.css"}), nodeResolve(), rollupNodeResolve({ jsnext: true, preferBuiltins: true, browser: true }),
+    rollupJson()]
 }
--- src/components/portfolioSideMenu/portfolioSideMenu.js
+++ src/components/portfolioSideMenu/portfolioSideMenu.js
@@ -1,5 +1,6 @@
 import { AbstractDiv } from '../../common/abstractDiv';
 import './portfolioSideMenu.css'
+import axios from 'axios'
 
 export class portfolioSideMenu extends AbstractDiv {
 
@@ -7,12 +8,36 @@ export class portfolioSideMenu extends AbstractDiv {
         super();
     }
 
+    getBTC() {
+        let response = null;
+        new Promise(async (resolve, reject) => {
+            try {
+                response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=BTC', {
+                headers: {
+                    'X-CMC_PRO_API_KEY': 'b589e3cd-a710-4e94-9b91-1f03e7bc3e47',
+                },
+                });
+            } catch(ex) {
+                response = null;
+                // error
+                console.log(ex);
+                reject(ex);
+            }
+            if (response) {
+                // success
+                const json = response.data;
+                console.log(json.data.BTC[0].quote);
+                resolve(json);
+            }
+        });
+    }
+
     render() {
         this.el.classList.add('portfolio-side-menu');
         this.el.innerHTML = `
         here will be list of all your portfolios
         `
-        
+        this.getBTC()
 
         return this.el
     }
